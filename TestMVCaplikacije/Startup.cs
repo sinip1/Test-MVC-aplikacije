1de35c45d7c3f4a68b62b88dd6038b98396492fe
sinip <sinip@SINIP-LAPTOP>
2017-03-13 17:14:03 +0100

    Add project files.

---
+++ TestMVCaplikacije/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestMVCaplikacije.Startup))]
+namespace TestMVCaplikacije
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
